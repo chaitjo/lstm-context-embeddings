e6c0b38a6726b64c03251160688cdb2adfbbac78
Chaitanya <ckjoshi9@gmail.com>
2016-07-27 17:26:25 +0800

    Cleanup

--- w2v_init.py
+++
@@ -1,7 +0,0 @@
-from gensim.models import Word2Vec
-
-vocab = Word2Vec.load_word2vec_format('/tmp/vectors.bin', binary=True)
-vocab.init_sims(replace=True)
-
-vocab[word] # raw numpy vector of a word
-
