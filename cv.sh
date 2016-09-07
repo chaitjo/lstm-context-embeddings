b9a1f85a72febf4f8feeb75cb99943c6e6b84ef1
chaitjo <ckjoshi9@gmail.com>
2016-09-07 10:35:02 +0800

    Just getting CV outputs to laptop...

--- cv.sh
+++ cv.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 cd ~/lstm-context-embeddings
-python cnn-text-classification-tf/train.py 2>&1 | tee outputs/output-cnn.txt
-python train.py 2>&1 | tee outputs/output-normal.txt
 for i in `seq 0 9`;
 do
 	python train_cv.py --cv_index=$i 2>&1 | tee outputs/output-cv-$i.txt
--- train_cv.py
+++ train_cv.py
@@ -19,11 +19,11 @@ tf.flags.DEFINE_integer("hidden_dim", 300, "Dimensionality of hidden layer in LS
 tf.flags.DEFINE_string("filter_sizes", "3,4,5", "Comma-separated filter sizes (default: '3,4,5')")
 tf.flags.DEFINE_integer("num_filters", 100, "Number of filters per filter size (default: 100)")
 tf.flags.DEFINE_float("dropout_keep_prob", 0.5, "Dropout keep probability (default: 0.5)")
-tf.flags.DEFINE_float("l2_reg_lambda", 0.15, "L2 regularizaion lambda (default: 0.15)")
+tf.flags.DEFINE_float("l2_reg_lambda", 0, "L2 regularizaion lambda (default: 0.15)")
 
 # Training parameters
 tf.flags.DEFINE_integer("batch_size", 50, "Batch Size (default: 50)")
-tf.flags.DEFINE_integer("num_epochs", 100, "Number of training epochs (default: 5)")
+tf.flags.DEFINE_integer("num_epochs", 30, "Number of training epochs (default: 5)")
 tf.flags.DEFINE_integer("evaluate_every", 100, "Evaluate model on dev set after this many steps (default: 100)")
 tf.flags.DEFINE_integer("checkpoint_every", 100, "Save model after this many steps (default: 100)")
 tf.flags.DEFINE_integer("cv_index", 0, "Cross validation index (default: 0)")
