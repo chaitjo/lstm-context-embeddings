616e8c802b21625a3aa87e72db0b770ac0349bbf
chaitjo <ckjoshi9@gmail.com>
2016-12-24 12:27:02 +0800

    Working on tflearn!

--- tflearn/model.py
+++ tflearn/model.py
@@ -7,6 +7,7 @@ from tflearn.datasets import imdb
 from tflearn.layers.core import input_data, dropout, fully_connected
 from tflearn.layers.embedding_ops import embedding
 from tflearn.layers.recurrent import bidirectional_rnn, BasicLSTMCell
+from tflearn.activations import linear, sigmoid, relu, softmax
 from tflearn.layers.merge_ops import merge
 from tflearn.layers.conv import conv_1d, global_max_pool
 from tflearn.layers.estimator import regression
@@ -14,8 +15,8 @@ from tflearn.layers.estimator import regression
 
 # IMDB Dataset loading
 train, test, _ = imdb.load_data(
-	path='imdb.pkl', 
-	n_words=10000,
+    path='imdb.pkl', 
+    n_words=10000,
     valid_portion=0.1)
 trainX, trainY = train
 testX, testY = test
@@ -32,31 +33,32 @@ testY = to_categorical(testY, nb_classes=2)
 
 
 # Building convolutional network
-network = input_data(shape=[None, 100], name='input')	# [sample, word]
+network = input_data(shape=[None, 100], name='input')   # [sample, word]
 
 network = embedding(
-	network, 
-	input_dim=10000, 
-	output_dim=128, 
-	trainable=False)	# [sample, word, embedding dim]
+    network, 
+    input_dim=10000, 
+    output_dim=128, 
+    trainable=False)    # [sample, word, embedding dim]
 
 # incoming: [sample, timestep, input dim]
 network = bidirectional_rnn(
-	network, 
-	BasicLSTMCell(128, activation=None, inner_activation=tf.sigmoid), 
-	BasicLSTMCell(128, activation=None, inner_activation=tf.sigmoid), 
-	return_seq=True,
-	dynamic=True)
+    network, 
+    BasicLSTMCell(128, activation=linear, inner_activation=sigmoid), 
+    BasicLSTMCell(128, activation=linear, inner_activation=sigmoid), 
+    return_seq=True,
+    dynamic=True)
 # output: [sample, timestep, output dim], output is depth concat for fw and bw
+network = tf.pack(network, axis=1)
 
 fw_outputs, bw_outputs = tf.split(split_dim=2, num_split=2, value=network) 
 
 # network = merge([fw_outputs, bw_outputs], mode='elementwise_sum', axis=2)
 network = tf.add(fw_outputs, bw_outputs)
 
-branch1 = conv_1d(network, 128, 3, padding='valid', activation='relu', regularizer="L2")
-branch2 = conv_1d(network, 128, 4, padding='valid', activation='relu', regularizer="L2")
-branch3 = conv_1d(network, 128, 5, padding='valid', activation='relu', regularizer="L2")
+branch1 = conv_1d(network, 128, 3, padding='valid', activation=relu, regularizer="L2")
+branch2 = conv_1d(network, 128, 4, padding='valid', activation=relu, regularizer="L2")
+branch3 = conv_1d(network, 128, 5, padding='valid', activation=relu, regularizer="L2")
 
 network = merge([branch1, branch2, branch3], mode='concat', axis=1)
 
@@ -66,23 +68,22 @@ network = global_max_pool(network)
 
 network = dropout(network, 0.5)
 
-network = fully_connected(network, 2, activation='softmax')
+network = fully_connected(network, 2, activation=softmax)
 
 network = regression(
-	network, 
-	optimizer='adam', 
-	learning_rate=0.001, 
-	loss='categorical_crossentropy', 
-	name='target')
-
+    network, 
+    optimizer='adam', 
+    learning_rate=0.001, 
+    loss='categorical_crossentropy', 
+    name='target')
 
 # Training
 model = tflearn.DNN(network, tensorboard_verbose=0, tensorboard_dir='runs')
 model.fit(
-	trainX, 
-	trainY, 
-	validation_set=(testX, testY),
-	n_epoch = 5, 
-	shuffle=True,  
-	show_metric=True, 
-	batch_size=32)
+    trainX, 
+    trainY, 
+    validation_set=(testX, testY),
+    n_epoch = 5, 
+    shuffle=True,  
+    show_metric=True, 
+    batch_size=32)
