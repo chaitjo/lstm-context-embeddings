f6aa0098f1439040f2d0a48dd686feb9e56de7b0
Chaitanya Joshi <ckjoshi9@gmail.com>
2017-01-11 16:48:02 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-python tflearn/model.py
-python tflearn/cnn.py
-python tflearn/model.py --embedding_dim=300 --rnn_hidden_size=150 --num_filters=100
+python train.py --embedding_dim=50 --hidden_dim=30 --num_filters=20
+python .py --embedding_dim=50 --num_filters=20
 python tflearn/cnn.py --embedding_dim=300 --num_filters=100
-python train.py --word2vec=GoogleNews-vectors-negative300.bin
-python cnn-text-classification/train.py --word2vec=GoogleNews-vectors-negative300.bin
-
+python train.py --num_epochs=30 --word2vec=GoogleNews-vectors-negative300.bin
+python cnn-model/train.py --num_epochs=30 --word2vec=GoogleNews-vectors-negative300.bin
