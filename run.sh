dab62222ddc9315cbbed8915ad06a38bff567854
chaitjo <ckjoshi9@gmail.com>
2017-01-15 10:54:47 +0800

    run updates

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 python train.py --embedding_dim=50 --hidden_dim=30 --num_filters=20
-python .py --embedding_dim=50 --num_filters=20
-python tflearn/cnn.py --embedding_dim=300 --num_filters=100
+python cnn-model/train.py --embedding_dim=50 --num_filters=20
 python train.py --num_epochs=30 --word2vec=GoogleNews-vectors-negative300.bin
 python cnn-model/train.py --num_epochs=30 --word2vec=GoogleNews-vectors-negative300.bin
