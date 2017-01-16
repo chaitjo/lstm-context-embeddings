dc5d05c0993bd6fd6174b97e68a241de06b0aa28
Chaitanya Joshi <ckjoshi9@gmail.com>
2017-01-16 23:40:24 +0800

    Delete run.sh

--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-python train.py --embedding_dim=50 --hidden_dim=30 --num_filters=20
-python cnn-model/train.py --embedding_dim=50 --num_filters=20
-python train.py --num_epochs=30 --word2vec=GoogleNews-vectors-negative300.bin
-python cnn-model/train.py --num_epochs=30 --word2vec=GoogleNews-vectors-negative300.bin
